a1fb4332bff5b0f274e30f6035236d04be251965
quandinh10 <dinhquan1003@gmail.com>
2023-09-23 12:23:09 +0700

    move function update to main.c

--- src_code/Core/Inc/display7SEG.h
+++ src_code/Core/Inc/display7SEG.h
@@ -8,11 +8,9 @@
 #ifndef INC_DISPLAY7SEG_H_
 #define INC_DISPLAY7SEG_H_
 
-//const int MAX_LED = 4;
-extern int index_led;
-extern int led_buffer[4];
 
 void clearSignal();
 void display7SEG(int num);
-void update7SEG(int index);
+void clear7SEG();
+
 #endif /* INC_DISPLAY7SEG_H_ */
--- src_code/Core/Src/main.c
+++ src_code/Core/Src/main.c
@@ -91,8 +91,42 @@ int main(void)
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
   HAL_TIM_Base_Start_IT(&htim2);
+
   setTimer1(50);
   setTimer2(100);
+
+  const int MAX_LED = 4;
+  int index_led=0;
+  int led_buffer[4]={1,7,5,2};
+
+  void update7SEG(int index){
+		switch (index){
+			case 0:
+				clearSignal();
+				clear7SEG();
+				HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, RESET);
+				display7SEG(led_buffer[0]);
+				break;
+			case 1:
+				clearSignal();
+				clear7SEG();
+				HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, RESET);
+				display7SEG(led_buffer[1]);
+				break;
+			case 2:
+				clearSignal();
+				clear7SEG();
+				HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, RESET);
+				display7SEG(led_buffer[2]);
+				break;
+			case 3:
+				clearSignal();
+				clear7SEG();
+				HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, RESET);
+				display7SEG(led_buffer[3]);
+				break;
+		}
+	}
   /* USER CODE END 2 */
 
   /* Infinite loop */
