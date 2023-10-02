dc78cf28017881ccee492cd6e96d7c6daa2537b4
quandinh10 <dinhquan1003@gmail.com>
2023-10-02 17:36:35 +0700

    modify code

--- src_code/Core/Src/main.c
+++ src_code/Core/Src/main.c
@@ -98,29 +98,25 @@ int main(void)
 
   int hour = 15 , minute = 8 , second = 50;
   void update7SEG(int index){
-    	switch (index){
-    		case 0:
-    			clearSignal();
-    			display7SEG(led_buffer[0]);
-    			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, RESET);
-    			break;
-    		case 1:
-    			clearSignal();
-    			display7SEG(led_buffer[1]);
-    			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, RESET);
-    			break;
-    		case 2:
-    			clearSignal();
-    			display7SEG(led_buffer[2]);
-    			HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, RESET);
-   				break;
-    		case 3:
-    			clearSignal();
-    			display7SEG(led_buffer[3]);
-   				HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, RESET);
-   				break;
-    	}
-    }
+	  clearSignal();
+	  display7SEG(led_buffer[index]);
+	  switch (index){
+	  case 0:
+		  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, RESET);
+		  break;
+	  case 1:
+		  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, RESET);
+		  break;
+	  case 2:
+		  HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, RESET);
+		  break;
+	  case 3:
+		  HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, RESET);
+		  break;
+	  default:
+		  break;
+	  }
+  }
 
   void updateClockBuffer(){
 	  led_buffer[0]=hour/10;
