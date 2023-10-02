2966dd55d754271f4b4e4e57b97d8dab92cb9664
quandinh10 <dinhquan1003@gmail.com>
2023-10-02 17:30:42 +0700

    modify code

--- src_code/Core/Src/main.c
+++ src_code/Core/Src/main.c
@@ -95,39 +95,30 @@ int main(void)
   setTimer1(50);
   setTimer2(100);
 
-//  const int MAX_LED = 4;
+  const int MAX_LED = 4;
   int index_led=0;
   int led_buffer[4]={1,8,9,4};
 
   void update7SEG(int index){
-		switch (index){
-			case 0:
-				clearSignal();
-				display7SEG(led_buffer[0]);
-				HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, RESET);
-				break;
-			case 1:
-				clearSignal();
-				display7SEG(led_buffer[1]);
-				HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, RESET);
-				break;
-			case 2:
-				clearSignal();
-				display7SEG(led_buffer[2]);
-				HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, RESET);
-				break;
-			case 3:
-				clearSignal();
-				display7SEG(led_buffer[3]);
-				HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, RESET);
-				break;
-			default:
-				clearSignal();
-				display7SEG(led_buffer[0]);
-				HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, RESET);
-				break;
-		}
-	}
+	  clearSignal();
+	  display7SEG(led_buffer[index]);
+	  switch (index){
+	  case 0:
+		  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, RESET);
+		  break;
+	  case 1:
+		  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, RESET);
+		  break;
+	  case 2:
+		  HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, RESET);
+		  break;
+	  case 3:
+		  HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, RESET);
+		  break;
+	  default:
+		  break;
+	  }
+  }
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -139,7 +130,7 @@ int main(void)
 
 		  HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
 		  update7SEG(index_led++);
-		  if (index_led >= 4) index_led = 0;
+		  if (index_led >= MAX_LED) index_led = 0;
 	  }
 
 	  if (timer2_flag==1){
